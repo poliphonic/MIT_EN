71745e44735496b1e031b7e23701d38e07d5d457
poli <poliphonic@gmail.com>
2022-02-04 12:00:19 +0300

    4 Feb 2022

---
+++ bob.py
@@ -0,0 +1,11 @@
+# !/usr/bin/env python3.
+
+# Assume s is a string of lower case characters.
+# Write a program that prints the number of times the string 'bob'
+# occurs in s. For example, if s = 'azcbobobegghakl', then your program
+# should print
+# Number of times bob occurs is: 2
+
+s = 'azcbobobegghakl'
+bob = sum([1 for i in range(len(s)) if s[i:i + 3] == 'bob'])
+print('Number of times bob occurs is:', bob)
